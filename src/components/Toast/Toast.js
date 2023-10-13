062242660d923eba1354b9064bbaaa6f99e12a6e
Kyle Bachand <kbachand@sbcglobal.net>
2023-10-13 13:22:53 -0500

    Updated Toast component to use props from ToastPlayground to display proper icon variant and message for toast pop-up.

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -18,14 +18,17 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast() {
+function Toast({ message, variant }) {
+  const Icon = ICONS_BY_VARIANT[variant];
+
   return (
     <div className={`${styles.toast} ${styles.notice}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        {/* Change based on variant, state has been raised so can access*/}
+        <Icon size={24} />
       </div>
       <p className={styles.content}>
-        16 photos have been uploaded
+        {message}
       </p>
       <button className={styles.closeButton}>
         <X size={24} />
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,6 +4,8 @@ import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
 
+import Toast from '../Toast';
+
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
@@ -16,6 +18,11 @@ function ToastPlayground() {
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
+
+      <Toast 
+        message={message}
+        variant={variant}
+      />
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
